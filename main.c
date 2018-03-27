8e4c3f74a4c2f65b9dbf704460cd607b72ededd6
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-03-28 00:06:27 +0200

    implementacja stosowa

--- main.c
+++ main.c
@@ -3,8 +3,13 @@
 
 char* c;
 typedef enum { paren , bracket } what;
-what* t;
-size_t d;
+struct stoth {
+    what wha;
+    unsigned int ile;
+    struct stoth *poprz;
+};
+struct stoth *t;
+struct stoth *tt;
 unsigned int n;
 
 int main() {
@@ -12,57 +17,78 @@ int main() {
     getchar(); /*gets that newline*/
     c = malloc(sizeof(char));
     do {
-        t = NULL;
-        d = 0;
+        t = calloc(1,sizeof(struct stoth));
+        (*t).wha = paren;
+        (*t).ile = 0;
+        (*t).poprz = NULL;
+        tt = t;
         while(1) {
             *c = (char)getchar();
             switch (*c) {
                 case '(':
-                    if(t==NULL && d>0) goto koniecswitcha;
-                    d+= sizeof(what);
-                    t = realloc(t,d);
-                    *(t+d-sizeof(what)) = paren;
+                    if((*t).wha==paren) (*t).ile++;
+                    else {
+                        tt = t;
+                        t = calloc(1,sizeof(struct stoth));
+                        (*t).wha = paren;
+                        (*t).ile = 1;
+                        (*t).poprz = tt;
+                    }
                     goto koniecswitcha;
                 case '[':
-                    if(t==NULL && d>0) goto koniecswitcha;
-                    d+= sizeof(what);
-                    t = realloc(t,d);
-                    *(t+d-sizeof(what)) = bracket;
+                    if((*t).wha==bracket) (*t).ile++;
+                    else {
+                        tt = t;
+                        t = calloc(1,sizeof(struct stoth));
+                        (*t).wha = bracket;
+                        (*t).ile = 1;
+                        (*t).poprz = tt;
+                    }
                     goto koniecswitcha;
                 case ')':
-                    if(t==NULL) goto koniecswitcha;
-                    if(*(t+d-sizeof(what))==paren) {
-                        d-= sizeof(what);
-                        t = realloc(t,d);
-                        if(t==NULL) goto koniecswitcha;
-                    } else {
-                        /*free(t);*/
-                        goto koniecswitcha;
-                    }
+                    while((*t).ile==0)
+                        if((*t).poprz!=NULL) {
+                            tt = t;
+                            t=(*tt).poprz;
+                            free(tt);
+                        }
+                        else goto koniecno; //return No, bo za duzo zamykajacych parenow
+                    if((*t).wha==paren) (*t).ile--;
+                    else goto koniecno; //return No, bo nie zgadzaja sie
                     goto koniecswitcha;
                 case ']':
-                    if(t==NULL) goto koniecswitcha;
-                    if(*(t+d-sizeof(what))==bracket) {
-                        d-= sizeof(what);
-                        t = realloc(t,d);
-                        if(t==NULL) goto koniecswitcha;
-                    } else {
-                        /*free(t);*/
-                        goto koniecswitcha;
-                    }
+                    while((*t).ile==0)
+                        if((*t).poprz!=NULL) {
+                            tt = t;
+                            t=(*tt).poprz;
+                            free(tt);
+                        }
+                        else goto koniecno; //return No, bo za duzo zamykajacych bracketow
+                    if((*t).wha==bracket) (*t).ile--;
+                    else goto koniecno; //return No, bo nie zgadzaja sie
                     goto koniecswitcha;
                 case '\n':
                 default:
-                    if(t==NULL) goto koniecpetli;
-                    /*free(t);*/
-                    goto koniecpetli;
+                    while((*t).ile==0)
+                        if((*t).poprz!=NULL) {
+                            tt = t;
+                            t=(*tt).poprz;
+                            free(tt);
+                        }
+                        else {
+                            goto koniecyes;
+                        } //return Yes, mamy pusto
+                    goto koniecno; //return No, cos ciagle zostalo
             }
             koniecswitcha:
             continue;
         }
-        koniecpetli:
-        printf(d>0? "No" : "Yes");
-        putchar('\n');
+        koniecno:
+        free(t);
+        printf("No\n"); continue;
+        koniecyes:
+        free(t);
+        printf("Yes\n");
     } while (--n>0);
     free(c);
     return 0;
