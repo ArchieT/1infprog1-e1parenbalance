dfb010d42f340112274fdffe24c5c3788df18488
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2018-04-02 02:48:45 +0200

    new version of simple array stack version, without problematic code constructs

--- main.c
+++ main.c
@@ -1,69 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* c;
-typedef enum { paren , bracket } what;
-what* t;
-size_t d;
+char c;
+typedef enum {
+    paren, bracket
+} what;
+what *t;
+unsigned short int d;
 unsigned int n;
+typedef enum {
+    yes, no
+} whether;
+whether huh;
+
+void do_getchar_until_newline() {
+    do { c = (char) getchar(); } while (c != '\n');
+}
 
 int main() {
-    scanf("%d",&n);
-    getchar(); /*gets that newline*/
-    c = malloc(sizeof(char));
+    scanf("%d", &n);
+    c = (char) getchar(); /*gets that newline*/
     do {
         t = NULL;
         d = 0;
-        while(1) {
-            *c = (char)getchar();
-            switch (*c) {
+        while (1) {
+            c = (char) getchar();
+            what now = bracket;
+            huh = yes;
+            switch (c) {
                 case '(':
-                    if(t==NULL && d>0) goto koniecswitcha;
-                    d+= sizeof(what);
-                    t = realloc(t,d);
-                    *(t+d-sizeof(what)) = paren;
-                    goto koniecswitcha;
+                    now = paren;
                 case '[':
-                    if(t==NULL && d>0) goto koniecswitcha;
-                    d+= sizeof(what);
-                    t = realloc(t,d);
-                    *(t+d-sizeof(what)) = bracket;
+                    now = now == paren ? paren : bracket;
+                    t = realloc(t, (++d) * sizeof(what));
+                    t[d - 1] = now;
                     goto koniecswitcha;
                 case ')':
-                    if(t==NULL) goto koniecswitcha;
-                    if(*(t+d-sizeof(what))==paren) {
-                        d-= sizeof(what);
-                        t = realloc(t,d);
-                        if(t==NULL) goto koniecswitcha;
-                    } else {
-                        /*free(t);*/
-                        goto koniecswitcha;
-                    }
-                    goto koniecswitcha;
+                    now = paren;
                 case ']':
-                    if(t==NULL) goto koniecswitcha;
-                    if(*(t+d-sizeof(what))==bracket) {
-                        d-= sizeof(what);
-                        t = realloc(t,d);
-                        if(t==NULL) goto koniecswitcha;
-                    } else {
-                        /*free(t);*/
-                        goto koniecswitcha;
+                    now = now == paren ? paren : bracket;
+                    if (d == 0 || t[d - 1] != now) {
+                        free(t);
+                        do_getchar_until_newline();
+                        huh = no;
+                        goto koniecpetli;
                     }
+                    t = realloc(t, (--d) * sizeof(what));
                     goto koniecswitcha;
                 case '\n':
                 default:
-                    if(t==NULL) goto koniecpetli;
-                    /*free(t);*/
+                    free(t);
+                    huh = d == 0 ? yes : no;
                     goto koniecpetli;
             }
             koniecswitcha:
             continue;
         }
         koniecpetli:
-        printf(d>0? "No" : "Yes");
+        switch (huh) {
+            case yes:
+                printf("Yes");
+                break;;
+            case no:
+                printf("No");
+                break;;
+        }
         putchar('\n');
-    } while (--n>0);
-    free(c);
+    } while (--n > 0);
     return 0;
 }
